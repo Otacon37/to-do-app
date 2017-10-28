9be716f0d53db2c8efcfa2d9a1c60978b17aea04
Dan Balliet <dandibiasio37@gmail.com>
2017-10-27 20:48:19 -0400

    checkpoint-8-coompleted

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 function onReady (){
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
   const deleteButton = document.getElementById('deleteBtn')
 
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if(!newToDoText) {return}
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value='';
+
+    renderTheUI(toDos);
+  }
+
+  function renderTheUI(toDos) {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    //get the text
-    let title = newToDoText.value;
-
-    //create a new li
-    let newLi = document.createElement('li');
-
-    //create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = 'checkbox';
-
-    //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
-
-
+    createNewToDo();
   });
-
 
   deleteButton.addEventListener('click', event => {
       var itemList = document.getElementById('toDoList');
@@ -55,6 +56,8 @@ function onReady (){
       toBeDeleted.pop().remove();
     }
     });
+
+  renderTheUI(toDos);
 
 
 }
